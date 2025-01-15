c655744c846b23996f5c1b1ce6e24064db2d58a3
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2025-01-15 23:50:55 +0530

    Added the model logic for the schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -6,5 +6,23 @@ const userSchema = new mongoose.Schema({
     email:{type:String,require:true,unique:true,lowercase:true},
     createdAt:{ type: Date, default: Date.now }
 })
+const organizationSchema = new mongoose.Schema({
+    name:{type:String,required:true},
+    password:{type:String,required:true},
+    user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+
+})
+
+const feedbackSchema = new mongoose.Schema({
+    content: { type: String, required: true },
+    rating: { type: Number, min: 1, max: 5 },
+    organization: { type: mongoose.Schema.Types.ObjectId, ref: 'Organization', required: true },
+    submitterName: { type: String },
+    submitterEmail: { type: String },
+    date: { type: Date, default: Date.now }
+})
 const User = mongoose.model('User',userSchema)
-module.exports=User
+const Organization = mongoose.model('Organization', organizationSchema);
+const Feedback = mongoose.model('Feedback', feedbackSchema);
+
+module.exports={User, Organization, Feedback }
